47d116051912b8121c172fb3cc5cdb7dac114a52
Andrii Senkiv <ezfero@gmail.com>
2016-10-30 13:09:02 +0200

    Removed unused imports

--- app/src/main/java/com/silgrid/barcodegenerator/MainActivity.java
+++ app/src/main/java/com/silgrid/barcodegenerator/MainActivity.java
@@ -3,20 +3,9 @@ package com.silgrid.barcodegenerator;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.silgrid.barcodegenerator.generator.BarcodeGenerator;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
